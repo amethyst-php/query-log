7df07423e95e7553003cdb47e633423a34e38683
railken <railken@protonmail.ch>
2018-10-22 00:37:20 +0200

    improve clean query test

--- tests/Commands/CleanQueryTest.php
+++ tests/Commands/CleanQueryTest.php
@@ -10,10 +10,12 @@ class CleanQueryTest extends BaseTest
 {
     public function testCommand()
     {
+        
         // Create a record
         $m = new QueryLogManager();
         $result = $m->create(QueryLogFaker::make()->parameters());
         $this->assertEquals(true, $result->ok());
+        config(['amethyst.query-log.min_time' => 0, 'amethyst.query-log.min_queries' => 0]);
 
         $resource = $result->getResource();
         $resource->created_at = (new \DateTime())->modify('-11 days');
@@ -24,5 +26,7 @@ class CleanQueryTest extends BaseTest
         $this->artisan('amethyst:query-log:clean');
 
         $this->assertEquals(null, $m->getRepository()->findOneById($resource->id));
+
+        $this->app->terminate();
     }
 }
