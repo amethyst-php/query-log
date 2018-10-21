2e6ea45f1d241703321cdb01812534ed0d85c2b3
railken <railken@protonmail.ch>
2018-10-22 00:37:06 +0200

    add listener logger test

---
+++ tests/Listeners/LoggerTest.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Railken\Amethyst\Tests\Listeners;
+
+use Railken\Amethyst\Managers\QueryLogManager;
+use Railken\Amethyst\Tests\BaseTest;
+
+class LoggerTest extends BaseTest
+{
+    public function testLogQuery()
+    {
+        $manager = new QueryLogManager();
+
+        config(['amethyst.query-log.min_time' => 0, 'amethyst.query-log.min_queries' => 0]);
+
+        // Perform a simple query
+        $manager->getRepository()->newQuery()->where('id', 1)->get();
+
+        $this->assertEquals(1, 1);
+        
+        $this->app->terminate();
+    }
+}
