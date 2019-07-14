5c02663d10395b9c1ec6e42eca7138f2f9e0fb2f
railken <railken@protonmail.ch>
2019-07-14 13:05:00 +0200

    phpdoc

--- src/Managers/QueryLogManager.php
+++ src/Managers/QueryLogManager.php
@@ -5,6 +5,14 @@ namespace Amethyst\Managers;
 use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
+/**
+ * @method \Amethyst\Models\QueryLog newEntity()
+ * @method \Amethyst\Schemas\QueryLogSchema getSchema()
+ * @method \Amethyst\Repositories\QueryLogRepository getRepository()
+ * @method \Amethyst\Serializers\QueryLogSerializer getSerializer()
+ * @method \Amethyst\Validators\QueryLogValidator getValidator()
+ * @method \Amethyst\Authorizers\QueryLogAuthorizer getAuthorizer()
+ */
 class QueryLogManager extends Manager
 {
     use ConfigurableManager;
