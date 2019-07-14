564c693a84e74a135a036125c024e3eed193aa82
railken <railken@protonmail.ch>
2019-07-14 13:11:23 +0200

    fix style

--- src/Managers/QueryLogManager.php
+++ src/Managers/QueryLogManager.php
@@ -6,12 +6,12 @@ use Amethyst\Common\ConfigurableManager;
 use Railken\Lem\Manager;
 
 /**
- * @method \Amethyst\Models\QueryLog newEntity()
- * @method \Amethyst\Schemas\QueryLogSchema getSchema()
+ * @method \Amethyst\Models\QueryLog                 newEntity()
+ * @method \Amethyst\Schemas\QueryLogSchema          getSchema()
  * @method \Amethyst\Repositories\QueryLogRepository getRepository()
- * @method \Amethyst\Serializers\QueryLogSerializer getSerializer()
- * @method \Amethyst\Validators\QueryLogValidator getValidator()
- * @method \Amethyst\Authorizers\QueryLogAuthorizer getAuthorizer()
+ * @method \Amethyst\Serializers\QueryLogSerializer  getSerializer()
+ * @method \Amethyst\Validators\QueryLogValidator    getValidator()
+ * @method \Amethyst\Authorizers\QueryLogAuthorizer  getAuthorizer()
  */
 class QueryLogManager extends Manager
 {
